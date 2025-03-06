1382811f18da595e7ff05ba898d255a77d086abd
BrunoVieira005 <brunorv2005@gmail.com>
2025-03-06 01:58:48 -0300

    Commit SistemaMedida.java

---
+++ Controle de Fluxo/src/SistemaMedida.java
@@ -0,0 +1,20 @@
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String medida = "M";
+
+        // Switch Case
+        switch (medida) {
+            case "M":
+                System.out.println("MÉDIA");
+                break;
+            case "P":
+                System.out.println("PEQUENA");
+                break;
+            case "G":
+                System.out.println("GRANDE");
+                break;
+            default:
+                System.out.println("Indefinido");
+        }
+    }
+}
