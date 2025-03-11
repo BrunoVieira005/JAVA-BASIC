5d75be59df47c108d05feb872ba3668974ddd534
BrunoVieira005 <brunorv2005@gmail.com>
2025-03-11 17:14:45 -0300

    add ResultadoEscolar in package estruturas.condicionais

---
+++ Controle de Fluxo/src/estruturas/condicionais/ResultadoEscolar.java
@@ -0,0 +1,24 @@
+package estruturas.condicionais;
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+
+        // Condicional encadeada
+        int nota = 8;
+
+        if (nota >= 7) 
+            System.out.println("Aprovado");
+
+        else if (nota >= 5 && nota < 7) 
+            System.out.println("Prova de Recuperação");
+            
+        else 
+            System.out.println("Reprovado");
+        
+        // Condição ternária
+
+        int nota_2 = 4;
+
+        String resultado = nota_2 >= 7 ? "Aprovado" : nota_2 >=5 && nota_2 < 7 ? "Recuperação" :"Reprovado";
+        System.out.println(resultado);
+    }
+}
